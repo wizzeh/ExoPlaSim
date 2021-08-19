5a128adc3d2e5b360aa27cbbbecb3ab5abea4d17
Adiv Paradise <paradise@astro.utoronto.ca>
2021-08-18 22:34:10 -0400

    Fixing compatibility issues with versions of python3 and f2py3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
  
 setup(
     name='exoplasim',
-    version='3.0.0a13',
+    version='3.0.0a14',
     packages=['exoplasim',],
     zip_safe=False,
     install_requires=["numpy","matplotlib","scipy"],
