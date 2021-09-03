f1a54dc15ebd762e5a20400669ebfbf59025e55e
Adiv Paradise <paradise@astro.utoronto.ca>
2021-09-03 17:52:54 -0500

    Removed pumax and GUI from compile step; this removes dependence on X11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
  
 setup(
     name='exoplasim',
-    version='3.0.0a25',
+    version='3.0.0a26',
     packages=['exoplasim',],
     zip_safe=False,
     install_requires=["numpy>=1.16","matplotlib","scipy"],
