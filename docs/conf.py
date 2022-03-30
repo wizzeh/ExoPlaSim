cc9130d0cfbce1d4c7d6c82d62ffe65d97aa3388
Adiv Paradise <paradise@astro.utoronto.ca>
2022-03-30 11:39:36 -0400

    Fixing missing ozone parameters in pRT.image

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2020, Adiv Paradise'
 author = 'Adiv Paradise'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.11a3'
+release = '3.0.11a4'
 
 
 # -- General configuration ---------------------------------------------------
