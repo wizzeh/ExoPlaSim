8d482dfa051b290e889b121380daf96ba48388b3
Ellie High <6687206+Wizzeh@users.noreply.github.com>
2022-09-25 20:07:55 -0700

    debugging

--- exoplasim/plasim/src/radmod.f90
+++ exoplasim/plasim/src/radmod.f90
@@ -1346,7 +1346,7 @@
       if (nfixed==1) then
         if (mypid==NROOT) fixedlon = fixedlon + desync*mpstep
         call mpbcr(fixedlon)
-        adj_fixedlon = fixedlon - (360. * deltalamb / TWOPI)
+        adj_fixedlon = -360. * deltalamb / TWOPI !fixedlon - (360. * deltalamb / TWOPI)
         zrtim = TWOPI
         zmins = 1.0 - (adj_fixedlon/360.)  !Think about how to fix this: there's a dep
                                        !on rotspd. Maybe zrtim = TWOPI/1440.0?
